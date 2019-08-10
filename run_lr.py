b5b9d1d65602b78862b47b4a39385c155184ea7a
Jingzhi Zhang <zjz42027@gmail.com>
2019-08-10 12:48:49 +0800

    update: change default optimizer from bgd to lsm of lr

--- run_lr.py
+++ run_lr.py
@@ -1,10 +1,10 @@
 import sys
-from common import *
 
+from common import *
 
 if __name__ == '__main__':
     X_train, Y_train, X_test, Y_test = csv2dataset(CSV_FILE_NAME, NUM_TESTING_ITEMS)
-    weights, time_cost = train_model(X_train, Y_train, optimizer='bgd')
+    weights, time_cost = train_model(X_train, Y_train, optimizer='lsm')
     test_model(X_test, Y_test, weights, time_cost)
     plot_prediction(X_test, Y_test, weights)
 else:
